2f6570cea3d5c205b8aa371009154bfd8d1b357f
dawam23 <dawamikhsan23@gmail.com>
2024-02-14 23:48:40 +0700

    users index alignment

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -86,7 +86,8 @@
                                                     href="{{ route('users.edit', Crypt::encrypt($user->id)) }}">
                                                     {{ __('Edit') }}
                                                 </a>
-                                                <a class="dropdown-item" href="#"data-action="{{ route('users.destroy', Crypt::encrypt($user->id)) }}"
+                                                <a class="dropdown-item" href="#"
+                                                    data-action="{{ route('users.destroy', Crypt::encrypt($user->id)) }}"
                                                     data-name="{{ $user->name }}" data-bs-toggle="modal"
                                                     data-bs-target="#delete-user">
                                                     {{ __('Delete') }}
