fe9daa806542e4b90818fc231d5950e855596532
dawam23 <dawamikhsan23@gmail.com>
2024-02-17 16:35:27 +0700

    Update auth view & errors view

--- resources/views/auth/confirm-password.blade.php
+++ resources/views/auth/confirm-password.blade.php
@@ -16,7 +16,7 @@
             <p class="text-muted mb-4">{{ __('Please confirm your password before continuing.') }}</p>
 
             <div class="mb-3">
-                <label class="form-label">
+                <label class="form-label required">
                     {{ __('Password') }}
                     @if (Route::has('password.request'))
                     <span class="form-label-description">
@@ -24,7 +24,12 @@
                     </span>
                     @endif
                 </label>
-                <input type="password" name="password" class="form-control form-control-user @error('password') is-invalid @enderror" placeholder="{{ __('Enter Password') }}">
+
+                {!! Form::password('password', [
+                'class' => 'form-control form-control-user' . ( $errors->has('password' ? ' is-invalid' : '') ),
+                'placeholder'=> __('Enter Password')
+                ]) !!}
+
                 @error('password')
                 <div class="invalid-feedback">{{ $message }}</div>
                 @enderror
--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -16,24 +16,51 @@
             <h2 class="card-title text-center mb-4">{{ __('Reset Password') }}</h2>
 
             <div class="mb-3">
-                <label class="form-label">{{ __('Email address') }}</label>
-                <input type="email" name="email" value="{{ old('email', $request->email) }}" class="form-control @error('email') is-invalid @enderror" placeholder="{{ __('Email Address') }}" required autocomplete="email" autofocus>
+
+                {!! Form::label('email', __('Email address'), [
+                'class' => 'form-label required'
+                ]) !!}
+
+                {!! Form::email('email', old('email', $request->email), [
+                'class' => 'form-control' . ( $errors->has('email') ? ' is-invalid' : '' ),
+                'placeholder' => __('Email Address'),
+                'required',
+                'autocomplete' => 'email',
+                'autofocus'
+                ]) !!}
+
                 @error('email')
                 <div class="invalid-feedback">{{ $message }}</div>
                 @enderror
             </div>
 
             <div class="mb-3">
-                <label class="form-label">{{ __('New Password') }}</label>
-                <input type="password" name="password" class="form-control @error('password') is-invalid @enderror" placeholder="{{ __('New Password') }}">
+
+                {!! Form::label('password', __('New Password'), [
+                'class' => 'form-label required'
+                ]) !!}
+
+                {!! Form::password('password', [
+                'class' => 'form-control' . ( $errors->has('password') ? ' is-invalid' : ''),
+                'placeholder' => __('New Password')
+                ]) !!}
+
                 @error('password')
                 <div class="invalid-feedback">{{ $message }}</div>
                 @enderror
             </div>
 
             <div class="mb-3">
-                <label class="form-label">{{ __('Repeat New Password') }}</label>
-                <input type="password" name="password_confirmation" class="form-control @error('password_confirmation') is-invalid @enderror" placeholder="{{ __('Repeat New Password') }}">
+
+                {!! Form::label('password', __('Repeat New Password'), [
+                'class' => 'form-label required'
+                ]) !!}
+
+                {!! Form::password('password_confirmation', [
+                'class' => 'form-control' . ( $errors->has('password_confirmation') ? ' is-invalid' : ''),
+                'placeholder' => __('Repeat New Password')
+                ]) !!}
+
                 @error('password_confirmation')
                 <div class="invalid-feedback">{{ $message }}</div>
                 @enderror
